8584b6f072437110b8b28a1230d2a6513fc69de2
Micheal24622111 <akindelebode70@gmail.com>
2024-10-03 11:33:39 -0500

    third commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import createError from 'http-errors'
 import initDB from './initDB.js'
 dotenv.config()
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
